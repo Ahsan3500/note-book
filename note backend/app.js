5afc911e057015db0a5d66e2c9eb2d9999154404
Ahsan Ghaffar <ahsanghaffar35@gmail.com>
2026-03-25 18:53:00 +0500

    Remove cors references to fix deploy

--- note backend/app.js
+++ note backend/app.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -21,7 +20,6 @@ mongoose
     logger.error('error connection to MongoDB:', error.message)
   })
 
-app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
